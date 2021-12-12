6de3acc96a83a8f907e2fc2373c3de7ce2a55ed5
Justin Tiu <justintiu131@gmail.com>
2021-12-12 21:53:19 +0800

    finalle 404

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,15 +5,9 @@ const NavBar = () => {
                 <div class="container-fluid">
                     <a class="navbar-brand" href="/myfirstreact">Amogus</a>
                     <div class="collapse navbar-collapse" id="navbarNav">
-
-                            
                                 <a class="nav-link" href="/myfirstreact">Home</a>
-
                                 <a class="nav-link" href="/myfirstreact/employees">Employees</a>
-
->
                                 <a class="nav-link" href="/myfirstreact/add">Add Employee</a>
-
                     </div>
                 </div>
             </nav>
