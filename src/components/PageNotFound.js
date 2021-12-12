4362eb10330117f003b2a0177b680588f46589f0
Justin Tiu <justintiu131@gmail.com>
2021-12-12 20:48:41 +0800

    Update PageNotFound.js

--- src/components/PageNotFound.js
+++ src/components/PageNotFound.js
@@ -2,6 +2,7 @@ const PageNotFound =() =>{
     return(
         <div>
             <h1>Page Not Found!</h1>
+            <img src="https://thumbs.gfycat.com/WanLeftBuffalo-max-1mb.gif" alt="Nothing to see here.gif"/>
         </div>
     )
 }
